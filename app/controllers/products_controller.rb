30f68e2477836b9a336b6f50b3aece5ab50d2d7f
gtar69 <chihweichang69@gmail.com>
2014-10-30 19:10:42 +0800

    Google map - add marker

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
+
+
   def index
+
+    #geocoded_by "Lane 140, Yucheng St, Nangang District, Taipei City"
     #@hash = Gmaps4rails.build_markers(@users) do |user, marker|
     #  marker.lat user.latitude
     #  marker.lng user.longitude
